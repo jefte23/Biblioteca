119fc239d90b1fa85d020c8718ab70a016d26ca1
ra21750454 <ra21750454@AN01201617077088.dinfor.uniceub>
2017-10-19 20:47:39 -0200

    1º commit

---
+++ Biblioteca/src/model/Livro.java
@@ -0,0 +1,57 @@
+package model;
+
+public class Livro {
+
+	// Atributos
+	private String titulo;
+	private String autor;
+	private String editora;
+	private String anoLancamento;
+	private String ISBN;
+	
+	@Override
+	public String toString() {
+		return "Livro [titulo=" + titulo + ", autor=" + autor + ", editora=" + editora + ", anoLancamento="
+				+ anoLancamento + ", ISBN=" + ISBN + "]";
+	}
+	public Livro(String titulo, String autor, String editora, String anoLancamento, String iSBN) {
+		super();
+		this.titulo = titulo;
+		this.autor = autor;
+		this.editora = editora;
+		this.anoLancamento = anoLancamento;
+		ISBN = iSBN;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	public String getEditora() {
+		return editora;
+	}
+	public void setEditora(String editora) {
+		this.editora = editora;
+	}
+	public String getAnoLancamento() {
+		return anoLancamento;
+	}
+	public void setAnoLancamento(String anoLancamento) {
+		this.anoLancamento = anoLancamento;
+	}
+	public String getISBN() {
+		return ISBN;
+	}
+	public void setISBN(String iSBN) {
+		ISBN = iSBN;
+	}
+	
+	
+}
